042323b0c1c0034114c95250a1b48b9b80d69ed9
Shashank Singh <101120408+Shankyking25@users.noreply.github.com>
2025-08-15 11:51:33 +0530

    Update axios.ts

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: "https://team-collaboration-platform-backend-2.onrender.com",
   withCredentials: true,
 });
 
